cc1de716909b37193bd3c864aafe07f1406d2684
forcowicz <forcowicz@gmail.com>
2022-06-26 04:05:03 +0200

    Changed states to refs in Form & file extensions

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,25 +1,13 @@
 import styles from "./Form.module.css";
 
 import { v4 as uuid } from "uuid";
-import { useState } from "react";
+import { useRef } from "react";
 import Button from "../UI/Button";
 
 const Form = function (props) {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [date, setDate] = useState("");
-
-  const firstNameHandler = e => {
-    setFirstName(e.target.value);
-  };
-
-  const lastNameHandler = e => {
-    setLastName(e.target.value);
-  };
-
-  const dateHandler = e => {
-    setDate(e.target.value);
-  };
+  const firstNameRef = useRef();
+  const lastNameRef = useRef();
+  const dateRef = useRef();
 
   const areInputsValid = function (...strings) {
     return !strings.some(s => s.trim().length === 0);
@@ -27,6 +15,10 @@ const Form = function (props) {
 
   const submitFormHandler = function (e) {
     e.preventDefault();
+
+    const firstName = firstNameRef.current.value;
+    const lastName = lastNameRef.current.value;
+    const date = dateRef.current.value;
 
     if (!areInputsValid(firstName, lastName, date)) return;
 
@@ -39,33 +31,29 @@ const Form = function (props) {
 
     props.onAddPerson(personData);
 
-    setFirstName("");
-    setLastName("");
-    setDate("");
+    firstNameRef.current.value = lastNameRef.current.value = dateRef.current.value = "";
   };
 
   return (<form className={styles.form} onSubmit={submitFormHandler}>
     <div className={styles["form-control"]}>
       <label htmlFor="firstName" className={styles["form-label"]}>Enter first name</label>
       <input type="text"
-             value={firstName}
-             onInput={firstNameHandler}
              placeholder="Enter first name"
              id="firstName"
-             autoComplete="off"/>
+             autoComplete="off"
+             ref={firstNameRef}/>
     </div>
     <div className={styles["form-control"]}>
       <label htmlFor="lastName" className={styles["form-label"]}>Enter last name</label>
       <input type="text"
-             value={lastName}
-             onInput={lastNameHandler}
              placeholder="Enter last name"
              id="lastName"
-             autoComplete="off"/>
+             autoComplete="off"
+             ref={lastNameRef}/>
     </div>
     <div className={styles["form-control"]}>
       <label htmlFor="date" className={styles["form-label"]}>Enter birth date</label>
-      <input type="date" value={date} onChange={dateHandler} id="date"/>
+      <input type="date" id="date" ref={dateRef}/>
     </div>
     <div className={styles["form-control"]}>
       <Button type="submit">Add Birthday</Button>
